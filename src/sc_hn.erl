255dd88f22343cf74bf9f7365f4c61cb76a789e9
John Haugeland <stonecypher@gmail.com>
2014-05-18 03:24:37 -0700

    improved fake data

--- src/sc_hn.erl
+++ src/sc_hn.erl
@@ -36,4 +36,10 @@ fake_stories(Count) ->
 
 fake_story() ->
 
-    don_martin:words().
+    { sc:rand_between(-20,20) + sc:rand(20) + sc:rand(50),
+      sc:unixtime() - sc:rand(400000), % about 5 days
+      don_martin:words(),
+      don_martin:word(),
+      don_martin:word() ++ don_martin:word() ++ sc:random_from([ ".com", ".net", ".org" ]),
+      sc:rand(50) + sc:rand(50) + sc:rand(50) + sc:rand(50)
+    }.
