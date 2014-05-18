9d903bf75c4c9da4f34ea8aa60aa7d51e3afac92
John Haugeland <stonecypher@gmail.com>
2014-05-18 03:07:26 -0700

    traction

--- src/sc_hn.erl
+++ src/sc_hn.erl
@@ -7,4 +7,25 @@
 
 -export([
 
+    fake_stories/0,
+      fake_stories/1,
+
+    fake_story/0
+
 ]).
+
+
+
+
+
+fake_stories() ->
+
+    fake_stories(100).
+
+
+
+
+
+fake_stories(Count) ->
+
+    todo.
